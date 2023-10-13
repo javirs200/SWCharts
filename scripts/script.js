8df3d38724da5772cabc0c00d35fb34f1fa16f1e
rsmax <fordrsmax@gmail.com>
2023-10-13 12:35:33 +0200

    add/js

---
+++ scripts/script.js
@@ -0,0 +1,77 @@
+
+
+// Ejercicio 1.
+// Titulos = results[i].title
+// Año = results[i].release_date
+let baseURL = "https://swapi.dev/api"
+function flims() {
+    fetch(baseURL + "/films/")
+        .then(response => response.json())
+        .then(flims => {
+            
+            let data = {}
+            data["labels"] = []
+            data["series"] = [[]]
+
+            for (const f of flims["results"]) {
+                data["labels"].push(f['title']);
+                data["series"][0].push(String(f["release_date"].split("-")[0]));//solo el year
+            }
+
+            console.log(data)
+            
+            var options = {
+                width: 850,
+                height: 400
+            };
+
+            
+            new Chartist.Line('#chart1', data, options); 
+
+        })
+   
+}
+
+// Ejercicio 2.
+// Personaje = results[i].name
+// Apariciones = results[i].films.length 
+function characters() {
+    fetch(baseURL + "/people/")
+        .then(response => response.json())
+        .then(characters => {
+            
+            let data = {}
+            data["labels"] = []
+            data["series"] = [[]]
+
+            for (const f of characters["results"]) {
+                data["labels"].push(f['name']);
+                data["series"][0].push(f['films'].length);
+            }
+
+            console.log(data)
+            
+            var options = {
+                width: 850,
+                height: 400
+            };
+
+            
+            new Chartist.Line('#chart2', data, options); 
+
+        })
+   
+}
+
+
+flims() 
+
+characters() 
+
+
+
+
+   
+
+
+
